c77f3891c1180895c10b34ebcda80c46a0957561
Luiz Carneiro <luiz.carneiro@yahoo.com.br>
2026-09-02 15:24:46 -0300

    Corrige validacao do identificador de interesse

--- functions/api/interesses/[id].js
+++ functions/api/interesses/[id].js
@@ -1,4 +1,4 @@
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{12}$/i;
+const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const DECISIONS = new Set(["in_intermediation", "rejected"]);
 
 function error(message, status = 400) {
